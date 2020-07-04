d87d77ff5bb5372a231e06aecd2545e17ea20481
jocimarweb <jocimarweb@gmail.com>
2020-07-04 18:28:44 -0300

    Commit - Casa - 04/07/20

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <!-- Head -->
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- CSS - Google (link) -->
+        <link
+            href="https://fonts.googleapis.com/css2?family=Lato&family=Montserrat&family=Oswald:wght@200;300;400;500;600;700&family=Raleway&family=Roboto&display=swap"
+            rel="stylesheet">
+
+        <!-- CSS - Personalizado -->
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        <title>Curso PHP 7 - COD3R </title>
+    </head> <!-- # Head -->
+    <!-- Body -->
+
+    <body>
+        <!-- # Body -->
+        <!-- Cabeçalho -->
+        <header class="cabecalho">
+            <h1>Curso PHP 7</h1>
+            <h2>Índice dos Exercícios</h2>
+        </header> <!-- # Cabeçalho -->
+
+        <!-- Conteúdo Principal -->
+        <main class="principal">
+            <div class="conteudo">
+                <!-- Nav - Modulos -->
+                <nav class="modulos">
+                    <!-- Nav - Modulo 01 -->
+                    <div class="modulo verde">
+                        <h3>Módulo 01</H3>
+                        <ul>
+                            <li><a href="#">Exercício A</a></li>
+                            <li><a href="#">Exercício B</a></li>
+                            <li><a href="#">Exercício C</a></li>
+                        </ul>
+                    </div> <!-- # Nav - Modulo 01 -->
+                    <!-- Nav - Modulo 02 -->
+                    <div class="modulo vermelho">
+                        <h3>Módulo 02</H3>
+                        <ul>
+                            <li><a href="#">Exercício A</a></li>
+                            <li><a href="#">Exercício B</a></li>
+                            <li><a href="#">Exercício C</a></li>
+                        </ul>
+                    </div> <!-- # Nav - Modulo 02 -->
+                    <!-- Nav - Modulo 03 -->
+                    <div class="modulo azul">
+                        <h3>Módulo 03</H3>
+                        <ul>
+                            <li><a href="#">Exercício A</a></li>
+                            <li><a href="#">Exercício B</a></li>
+                            <li><a href="#">Exercício C</a></li>
+                        </ul>
+                    </div> <!-- # Nav - Modulo 03 -->
+                    <!-- Nav - Modulo 04 -->
+                    <div class="modulo roxo">
+                        <h3>Módulo 04</H3>
+                        <ul>
+                            <li><a href="#">Exercício A</a></li>
+                            <li><a href="#">Exercício B</a></li>
+                            <li><a href="#">Exercício C</a></li>
+                        </ul>
+                    </div> <!-- # Nav - Modulo 04 -->
+                    <!-- Nav - Modulo 05 -->
+                    <div class="modulo laranja">
+                        <h3>Módulo 05</H3>
+                        <ul>
+                            <li><a href="#">Exercício A</a></li>
+                            <li><a href="#">Exercício B</a></li>
+                            <li><a href="#">Exercício C</a></li>
+                        </ul>
+                    </div> <!-- # Nav - Modulo 05 -->
+                    <!-- Nav - Modulo 06 -->
+                    <div class="modulo verde-escuro">
+                        <h3>Módulo 06</H3>
+                        <ul>
+                            <li><a href="#">Exercício A</a></li>
+                            <li><a href="#">Exercício B</a></li>
+                            <li><a href="#">Exercício C</a></li>
+                        </ul>
+                    </div> <!-- # Nav - Modulo 06 -->
+                    <!-- Nav - Modulo 07 -->
+                    <div class="modulo vermelho-escuro">
+                        <h3>Módulo 07</H3>
+                        <ul>
+                            <li><a href="#">Exercício A</a></li>
+                            <li><a href="#">Exercício B</a></li>
+                            <li><a href="#">Exercício C</a></li>
+                        </ul>
+                    </div> <!-- # Nav - Modulo 07 -->
+                    <!-- Nav - Modulo 08 -->
+                    <div class="modulo azul-escuro">
+                        <h3>Módulo 08</H3>
+                        <ul>
+                            <li><a href="#">Exercício A</a></li>
+                            <li><a href="#">Exercício B</a></li>
+                            <li><a href="#">Exercício C</a></li>
+                        </ul>
+                    </div> <!-- # Nav - Modulo 08 -->
+                    <!-- Nav - Modulo 09 -->
+                    <div class="modulo roxo-escuro">
+                        <h3>Módulo 09</h3>
+                        <ul>
+                            <li><a href="#">Exercício A</a></li>
+                            <li><a href="#">Exercício B</a></li>
+                            <li><a href="#">Exercício C</a></li>
+                        </ul>
+                    </div> <!-- # Nav - Modulo 09 -->
+                </nav> <!-- # Nav - Modulos -->
+            </div>
+        </main> <!-- # Conteúdo Principal -->
+
+        <!-- Rodapé  -->
+        <footer class="rodape">
+            COD3R & ALUNOS &copy; 2019
+        </footer> <!-- # Rodapé  -->
+    </body>
+
+</html>
